1a61401ee693186ae7b677c62f803c2e343a3283
keisuke ishizuka <keisuke.ishizuka@rakus-partners.co.jp>
2022-08-15 11:53:36 +0900

    first commit

---
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -0,0 +1,48 @@
+package junit.tutorial.ex01.e01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class StringUtilsTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test01() {
+		String result = StringUtils.toSnakeCase("aaa");
+		assertEquals("aaa", result,"toSnakeCase(\"aaa\")失敗");
+	}
+	
+	@Test
+	void test02() {
+		String result = StringUtils.toSnakeCase("HelloWorld");
+		assertEquals("hello_world", result,"toSnakeCase(\"HelloWorld\")失敗");
+	}
+	
+	@Test
+	void test03() {
+		String result = StringUtils.toSnakeCase("practiceJunit");
+		assertEquals("practice_junit", result,"toSnakeCase(\"practiceJunit\")失敗");
+	}
+
+}
---
+++ src/test/java/junit/tutorial/ex01/e03/CounterTest.java
@@ -0,0 +1,54 @@
+package junit.tutorial.ex01.e03;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class CounterTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+Counter counter = new Counter();
+	
+	@Test
+	void test1() {
+		int result = counter.increment();
+		assertEquals(1, result);
+	}
+	
+	@Test
+	void test2() {
+		counter.increment();
+		int result = counter.increment();
+		assertEquals(2, result);
+	}
+	
+	@Test
+	void test3() {
+		for(int i = 1; i <= 50; i++) {
+			counter.increment();
+		}
+		int result = counter.increment();
+		assertEquals(51, result);
+	}
+
+}
